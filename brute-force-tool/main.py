780a0077fde86b6134e238ed78b0acdc5d9fad3f
calreed-prof <calebkreed27@gmail.com>
2024-08-27 19:17:42 +0000

    base setup

--- brute-force-tool/main.py
+++ brute-force-tool/main.py
@@ -1,1 +1,10 @@
+# This file will aim to test my knowledge on brute forcing techniques
 
+target = "p"
+all_keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+pass_guess = []
+
+for key in all_keys:
+    if key == target:
+        print(f"target is {key}")
