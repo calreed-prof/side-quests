cc98766bffba8ab087d8bf8809985a9f22b44e31
calreed-prof <calebkreed27@gmail.com>
2024-08-28 09:30:37 -0600

    Update sum_seed.py

--- sum-seed/sum_seed.py
+++ sum-seed/sum_seed.py
@@ -1,1 +1,11 @@
+number = 111
 
+def sum_seed(number):
+    for i in range(1, number + 1):
+        newnum = i
+        for d in str(i):
+            newnum += int(d)
+        if newnum == number:
+            print(i)
+
+sum_seed(number)
